df1d690eac2b31c8cc07536b3fe1f7ca4c9de584
Lee JaeHyun <myleje@gmail.com>
2020-10-17 09:40:34 +0900

    .

---
+++ Assets/02.Scripts/TouchMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMove : MonoBehaviour
+{
+    private Transform tr;
+    
+    private Ray ray;
+    private RaycastHit hit;
+    private int floorLayer;
+    private Camera camera;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        floorLayer = LayerMask.NameToLayer("FLOOR");
+        camera = Camera.main;  //"MainCamera" 태그가 설정된 카메라를 리턴
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
